01bf631fe3ba8193c302e6f59570c70ae898d99f
hhiroki <h-hiroki@users.noreply.github.com>
2018-10-14 20:57:32 +0900

    phpのテストファイル作成

---
+++ src/test_example1-2-2.php
@@ -0,0 +1,10 @@
+<?php
+class MyTestCase extends PHPUnit_Framework_TestCase {
+	public function test_test1() {
+		$this->assertTrue(true);
+	}
+
+	public function test_test2() {
+		$this->assertTrue(true);
+	}
+}
