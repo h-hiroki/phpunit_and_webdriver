c9b7455712177ed5901da0e638f4bb06ff3d6231
hhiroki <h-hiroki@users.noreply.github.com>
2018-10-14 21:09:45 +0900

    assertメソッドをいくつかチェック

---
+++ src/test_example1-3.php
@@ -0,0 +1,27 @@
+<?php
+class MyTestCase extends PHPUnit_Framework_TestCase {
+	public function test_equal() {
+		$result = 1 + 2;
+		$this->assertEquals(3, $result);
+	}
+
+	public function test_notequal() {
+		$result = 1 + 2;
+		$this->assertNotEquals(100, $result);
+	}
+
+	public function test_string1() {
+		$string = "Hello" . "World";
+		$this->assertEquals("HelloWorld", $string);
+	}
+
+	public function test_string2() {
+		$string = "HelloWorld";
+		$this->assertStringStartsWith("H", $string);
+	}
+
+	public function test_string3() {
+		$string = "HelloWorld";
+		$this->assertContains("oWo", $string);
+	}
+}
