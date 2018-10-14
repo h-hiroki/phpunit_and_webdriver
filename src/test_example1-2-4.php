f58762f7fd979e4764a1b63cdaa1c58498e5a5bc
hhiroki <h-hiroki@users.noreply.github.com>
2018-10-14 21:01:44 +0900

    Update src/test_example1-2-4.php

---
+++ src/test_example1-2-4.php
@@ -0,0 +1,16 @@
+<?php
+class MyTestCase extends PHPUnit_Framework_TestCase {
+	/**
+	 * @test
+	 */
+	public function first_test1() {
+		echo "test1\n";
+	}
+
+	/**
+	 * @test
+	 */
+	public function second_test2() {
+		echo "test2\n";
+	}
+}
